91e77fa33bbca3b578e05d691c80cd173ee05f57
Vinay Kakkad <kakkadvinay25@gmail.com>
2021-06-01 06:36:12 +0000

    made changes for connection to chat app

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 
-from .views import (
-    temp_add, 
-    courses_views
-)
+from .views import temp_add, courses_views
 
 urlpatterns = [
     path("courses/", courses_views, name="courses"),
-    path("temp_add/", temp_add)
+    path("temp_add/", temp_add),
 ]
