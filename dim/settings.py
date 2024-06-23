b67989806daf565f7a71d8132ed5821ac08a406d
vinaykakkad <56934712+vinaykakkad@users.noreply.github.com>
2024-06-23 18:52:56 +0530

    Update settings for SMTP

--- dim/settings.py
+++ dim/settings.py
@@ -142,11 +142,11 @@ MEDIA_URL = "/media/"
 
 
 # For sending emails
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "mail.smtp2go.com"
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 
 
 # Authentication configurations
